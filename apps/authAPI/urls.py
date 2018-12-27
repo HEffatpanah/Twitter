756bf267f618b6373abede24e2a7e48e396464bc
aliasgharkhani <khani.ali1376@gmail.com>
2018-12-27 20:46:23 +0330

    attack detect created

--- apps/authAPI/urls.py
+++ apps/authAPI/urls.py
@@ -6,7 +6,8 @@ from apps.authAPI.views import views, APIviews
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    # path('login', views.login_page, name='login'),
+    path('login', views.login_page, name='login'),
+    path('logoutPage', views.logoutUser, name='logout'),
     path('api/v1/login', APIviews.login_page, name='index'),
     path('api/v1/tweet', APIviews.tweet, name='tweet'),
     path('', views.mainPage, name='home'),
@@ -16,9 +17,9 @@ urlpatterns = [
     path('api/v2/login', APIviews.login_page, name='login+token'),
     path('api/v2/tweet', APIviews.tweet, name='tweet+token'),
 
-    url(r'^$', auth_views.home, name='home'),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
-    url(r'^admin/', admin.site.urls),
+    # url(r'^$', auth_views.home, name='home'),
+    # url(r'^login/$', auth_views.login, name='login'),
+    # url(r'^logout/$', auth_views.logout, name='logout'),
+    # url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+    # url(r'^admin/', admin.site.urls),
 ]
